2722c9c81984137a518a6bebe7798da16ffff09f
Dan Lee <dan@leesolutions.net>
2013-02-24 17:00:39 -0800

    add custom devise mailer to send email asynchronously; use smtp vs sendmail

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,7 +65,19 @@ Gitlab::Application.configure do
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
 
-  config.action_mailer.delivery_method = :sendmail
+  config.action_mailer.default_url_options = {:host => "example.com"}
+
+  config.action_mailer.smtp_settings = {
+    :address              => "smtp.gmail.com",
+    :port                 => 587,
+    :domain               => 'example.com',
+    :user_name            => 'noreply@example.com',
+    :password             => 'p@ssw0rd',
+    :authentication       => 'plain',
+    :enable_starttls_auto => true
+  }
+
+  # config.action_mailer.delivery_method = :sendmail
   # Defaults to:
   # # config.action_mailer.sendmail_settings = {
   # #   :location => '/usr/sbin/sendmail',
